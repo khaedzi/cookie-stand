7e6e28c7069fe4850bdc26cb8f41ac165e7e8f91
khaedzi <khaled.ziadeh@yahoo.com>
2021-01-03 15:30:30 +0200

    add file

---
+++ js/app.js
@@ -0,0 +1,37 @@
+'use strict';
+var Seattle={
+    maxHourlyCustomers:65,
+    minHourlyCustomers:23,
+    averageCookiesPerCustomer:6.3,
+
+
+}
+var Tokyo={
+    maxHourlyCustomers:24,
+    minHourlyCustomers:3,
+    averageCookiesPerCustomer:1.2,
+    
+
+}
+
+var Dubai={
+    maxHourlyCustomers:38,
+    minHourlyCustomers:11,
+    averageCookiesPerCustomer:3.7,
+    
+
+}
+var Paris={
+    maxHourlyCustomers:38,
+    minHourlyCustomers:20,
+    averageCookiesPerCustomer:2.3,
+    
+
+}
+var Lima={
+    maxHourlyCustomers:16,
+    minHourlyCustomers:2,
+    averageCookiesPerCustomer:4.6,
+    
+
+}
